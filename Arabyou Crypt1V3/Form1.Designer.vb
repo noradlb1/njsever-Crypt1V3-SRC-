700decc66ae3632480a56844923a76d9efaba8bf
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-27 06:28:18 +0100

    njsever-Crypt1V3-SRC-
    njsever-Crypt1V3-SRC-

---
+++ Arabyou Crypt1V3/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1998 = New System.Windows.Forms.Label()
+        Me.Text34 = New System.Windows.Forms.TextBox()
+        Me.Text35 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.Yellow
+        Me.TextBox1.Location = New System.Drawing.Point(87, 4)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(285, 23)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Arabyou"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.ForeColor = System.Drawing.Color.Yellow
+        Me.TextBox2.Location = New System.Drawing.Point(87, 33)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(285, 212)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = resources.GetString("TextBox2.Text")
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.BackColor = System.Drawing.Color.Black
+        Me.NumericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.NumericUpDown1.ForeColor = System.Drawing.SystemColors.ActiveBorder
+        Me.NumericUpDown1.Location = New System.Drawing.Point(8, 197)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(75, 16)
+        Me.NumericUpDown1.TabIndex = 3
+        Me.NumericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1998
+        '
+        Me.Label1998.AutoSize = True
+        Me.Label1998.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1998.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Label1998.Location = New System.Drawing.Point(33, 121)
+        Me.Label1998.Name = "Label1998"
+        Me.Label1998.Size = New System.Drawing.Size(25, 15)
+        Me.Label1998.TabIndex = 39
+        Me.Label1998.Text = "TO"
+        '
+        'Text34
+        '
+        Me.Text34.BackColor = System.Drawing.SystemColors.WindowText
+        Me.Text34.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Text34.ForeColor = System.Drawing.Color.Aquamarine
+        Me.Text34.Location = New System.Drawing.Point(8, 95)
+        Me.Text34.Multiline = True
+        Me.Text34.Name = "Text34"
+        Me.Text34.Size = New System.Drawing.Size(75, 23)
+        Me.Text34.TabIndex = 36
+        Me.Text34.Text = "A"
+        Me.Text34.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Text35
+        '
+        Me.Text35.BackColor = System.Drawing.SystemColors.WindowText
+        Me.Text35.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Text35.ForeColor = System.Drawing.Color.Aquamarine
+        Me.Text35.Location = New System.Drawing.Point(8, 139)
+        Me.Text35.Multiline = True
+        Me.Text35.Name = "Text35"
+        Me.Text35.Size = New System.Drawing.Size(75, 23)
+        Me.Text35.TabIndex = 35
+        Me.Text35.Text = "@"
+        Me.Text35.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Black
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Lime
+        Me.Button2.Location = New System.Drawing.Point(8, 36)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 40
+        Me.Button2.Text = "Base64"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button22
+        '
+        Me.Button22.BackColor = System.Drawing.Color.Black
+        Me.Button22.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button22.ForeColor = System.Drawing.Color.Red
+        Me.Button22.Location = New System.Drawing.Point(8, 66)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(75, 23)
+        Me.Button22.TabIndex = 42
+        Me.Button22.Text = "Replace"
+        Me.Button22.UseVisualStyleBackColor = False
+        '
+        'Button18
+        '
+        Me.Button18.BackColor = System.Drawing.Color.Black
+        Me.Button18.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button18.ForeColor = System.Drawing.Color.Yellow
+        Me.Button18.Location = New System.Drawing.Point(8, 219)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(75, 23)
+        Me.Button18.TabIndex = 43
+        Me.Button18.Text = "Split"
+        Me.Button18.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Location = New System.Drawing.Point(8, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 44
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Black
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Yellow
+        Me.Button3.Location = New System.Drawing.Point(8, 168)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 45
+        Me.Button3.Text = "Code"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.ControlText
+        Me.ClientSize = New System.Drawing.Size(373, 246)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button22)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1998)
+        Me.Controls.Add(Me.Text34)
+        Me.Controls.Add(Me.Text35)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Arabyou Crypt1V3"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1998 As System.Windows.Forms.Label
+    Friend WithEvents Text34 As System.Windows.Forms.TextBox
+    Friend WithEvents Text35 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button22 As System.Windows.Forms.Button
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Arabyou Crypt1V3/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.CompilerServices
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.Drawing
+
+Imports System.IO.Compression
+Imports System.Runtime.CompilerServices
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Windows.Forms
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Label1998_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1998.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Text35_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Text35.TextChanged
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Me.Button1.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.Button3.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.TextBox1.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.Label1998.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.TextBox2.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.Text35.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.Button18.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.Button22.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+        Me.Button2.ForeColor = ColorTranslator.FromOle(Information.RGB(CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 255.0!)))), CInt(Math.Round(CDbl((VBMath.Rnd * 500.0!))))))
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim dialog As New OpenFileDialog
+        Dim dialog2 As OpenFileDialog = dialog
+        dialog2.Title = "Select EXE File"
+        dialog2.Filter = "(*.exe)|*.exe"
+        dialog2.ShowDialog()
+        dialog2 = Nothing
+        Me.TextBox1.Text = dialog.FileName
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.TextBox2.Text = Convert.ToBase64String(File.ReadAllBytes(Me.TextBox1.Text))
+    End Sub
+
+    Private Sub Button22_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button22.Click
+        Me.TextBox2.Text = Me.TextBox2.Text.Replace(Me.Text34.Text, Me.Text35.Text)
+    End Sub
+
+    Private Sub Button18_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button18.Click
+        Dim dialog2 As New FolderBrowserDialog
+        If (dialog2.ShowDialog = DialogResult.OK) Then
+            Dim str As String = Conversions.ToString(Me.NumericUpDown1.Value)
+            Dim text As String = Me.TextBox2.Text
+            Dim str4 As String = Conversions.ToString(CDbl((Conversions.ToDouble(Conversions.ToString([text].Length)) / Conversions.ToDouble(str))))
+            Dim str5 As String = Conversions.ToString(1)
+            Dim num2 As Integer = Conversions.ToInteger(str)
+            Dim i As Integer = 0
+            Do While (i <= num2)
+                Dim contents As String = Strings.Mid([text], Conversions.ToInteger(str5), Conversions.ToInteger(str4))
+                File.WriteAllText((dialog2.SelectedPath & "\P" & Conversions.ToString(i) & ".txt"), contents)
+                str5 = Conversions.ToString(CDbl((Conversions.ToDouble(str5) + Conversions.ToInteger(str4))))
+                i += 1
+            Loop
+        End If
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox2.Text = My.Resources.OMAR313
+    End Sub
+End Class
